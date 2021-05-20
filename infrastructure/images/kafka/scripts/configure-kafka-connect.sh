f30ad1f68f933a81d97212f4697c6ed110ac82bd
Pascal Holy <pascal@airy.co>
2021-05-20 12:22:52 +0200

    Add cp helm

--- infrastructure/images/kafka/scripts/configure-kafka-connect.sh
+++ infrastructure/images/kafka/scripts/configure-kafka-connect.sh
@@ -3,8 +3,8 @@ set -eo pipefail
 
 CONFIG=/etc/kafka/kafka-connect.properties
 
-echo "bootstrap.servers=kafka-headless:9092" >> $CONFIG
-
+echo "bootstrap.servers=${CONNECT_BOOTSTRAP_SERVERS}" >> $CONFIG
+echo "group.id=${CONNECT_GROUP_ID}" >> $CONFIG
 
 echo "Printing out the broker configuration"
 cat $CONFIG
